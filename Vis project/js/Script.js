ae0d274b2691519102ed14d0dab3c51f03ab7e99
M7mdQash <113006736+M7mdQash@users.noreply.github.com>
2024-02-03 00:18:31 +0300

    V0.4.4 changes some style

--- Vis project/js/Script.js
+++ Vis project/js/Script.js
@@ -8,3 +8,25 @@ function toggleDarkMode() {
         themeStyles.href = 'css/Dark-style.css';
     }
 }
+
+function navigateToPage(sectionId) {
+    // Prevent default action if it's necessary
+    // For a select dropdown, the default action does not need to be prevented.
+
+    // Find the element that corresponds to the id provided by the dropdown.
+    const targetElement = document.getElementById(sectionId);
+
+    if (targetElement) {
+        // Calculate the position of the target element.
+        const offset = 0; // Adjust this if you need some spacing above the target section.
+        const topPosition = targetElement.offsetTop - offset;
+
+        // Smooth scroll to the target element.
+        window.scrollTo({
+            top: topPosition,
+            behavior: 'smooth'
+        });
+    } else {
+        console.error("Element with id '" + sectionId + "' was not found.");
+    }
+}
