d6da9124b2ef3964e1275619299593038183ea3c
Feras-MQ <158749256+Feras-MQ@users.noreply.github.com>
2024-02-07 20:57:13 +0300

    Add files via upload

--- Vis project/js/Script.js
+++ Vis project/js/Script.js
@@ -15,3 +15,15 @@ function navigateToPage(sectionId) {
         section.scrollIntoView({ behavior: 'smooth', block: 'start' });
     }
 }
+
+const menu = document.getElementById("menu");
+const menuItems = document.querySelectorAll('.menu-item');
+
+menuItems.forEach((item, index) => {
+  item.addEventListener('mouseover', () => {
+    menu.dataset.activeIndex = index;
+    // Update background pattern and image based on active index
+    document.getElementById("menu-background-pattern").style.backgroundPosition = `${index * -25}% 0%`;
+    document.getElementById("menu-background-image").style.backgroundPosition = `center ${45 + (index * 5)}%`;
+  });
+});
